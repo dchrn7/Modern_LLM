2082f30d8a01577686d4ccfb7c2aded7c8564f88
MaelMontillet <mael.montillet@etu.toulouse-inp.fr>
2026-06-20 11:27:30 +0200

    Working but following jobs should run after the slurm time limit error

--- Project/scripts/cnn/run4_jobs.bash
+++ Project/scripts/cnn/run4_jobs.bash
@@ -1,8 +1,11 @@
-jid1=$(sbatch train.sbatch | awk '{print $4}') 
-jid2=$(sbatch --dependency=afterok:$jid1 train.sbatch | awk '{print $4}') 
-jid3=$(sbatch --dependency=afterok:$jid2 train.sbatch | awk '{print $4}') 
-jid4=$(sbatch --dependency=afterok:$jid3 train.sbatch | awk '{print $4}') 
-echo "$jid1 -> $jid2 -> $jid3 -> $jid4"
+jid1=$(sbatch train.sbatch | awk '{print $4}')
+jid2=$(sbatch --dependency=afterany:$jid1 train.sbatch | awk '{print $4}')
+jid3=$(sbatch --dependency=afterany:$jid2 train.sbatch | awk '{print $4}')
+jid4=$(sbatch --dependency=afterany:$jid3 train.sbatch | awk '{print $4}')
+jid5=$(sbatch --dependency=afterany:$jid4 train.sbatch | awk '{print $4}')
+jid6=$(sbatch --dependency=afterany:$jid5 train.sbatch | awk '{print $4}')
+jid7=$(sbatch --dependency=afterany:$jid6 train.sbatch | awk '{print $4}')
+jid8=$(sbatch --dependency=afterany:$jid7 train.sbatch | awk '{print $4}')
+jid9=$(sbatch --dependency=afterany:$jid8 train.sbatch | awk '{print $4}')
 
-
-#93221 -> 93222 -> 93223 -> 93224
+echo "$jid1 -> $jid2 -> $jid3 -> $jid4 -> $jid5 -> $jid6 -> $jid7 -> $jid8 -> $jid9"
